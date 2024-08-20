446df4b2fefcf5cbe1a8c96e1a572e7af19be38e
Gonzalo Cortés <gonzaloigcortescortes@gmail.com>
2024-08-20 20:34:46 +0000

    Terminado Basico

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+  const suitIndex = Math.floor(Math.random() * 4);
+  const numberIndex = Math.floor(Math.random() * 12);
+
+  const suit = suits[suitIndex];
+  const number = numbers[numberIndex];
+
+  document.querySelector(".top-suit").innerHTML = suits[suitIndex];
+  document.querySelector(".bottom-suit").innerHTML = suits[suitIndex];
+  document.querySelector(".number").innerHTML = numbers[numberIndex];
+  const color = suit === "♦" || suit === "♥" ? "red" : "black";
+  document.querySelector(".top-suit").style.color = color;
+  document.querySelector(".bottom-suit").style.color = color;
 };
